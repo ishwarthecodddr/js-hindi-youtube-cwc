5d569338350d384a9b7ff87b2a7a604ee931a41d
ishwarthecodddr <142937211+ishwarthecodddr@users.noreply.github.com>
2024-01-26 05:37:18 +0000

    datatypes tutorial

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+let a=3;
+const b=3; //cannot be changed
+var c =2;  //avoid using var because of it's problem with block scope and functional scope.
+console.table([a,b,c])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+"use Strict" // treat all JS code as new version.
+//alert("hello")  // cannot use like this because we are in nodejs not in browser
+
+console.log(typeof null)  //interview question . Q-> what is the type of null.
