73cfb86ad67f1db06108c7d86f661abfb776105c
ishwarthecodddr <142937211+ishwarthecodddr@users.noreply.github.com>
2024-01-26 12:52:17 +0000

    number learned

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,8 @@
+const name = new String("Ishwar")
+console.log(name)
+console.log(name.split(''))
+console.log(name[3])
+
+//String is object in javascript with key-value pairs .
+
+//for more explore https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String.
---
+++ 01_basics/05_number.js
@@ -0,0 +1,8 @@
+const num = 100;
+console.log(num)
+
+const number = new Number(100)
+console.log(number.toString().length)
+console.log(number.toFixed(2))  // precision value to two decimal places.
+console.log(Math.random()); //gives random values between 0-1
+console.log(Math.floor(Math.random()*10)+1)
