8caba7823fcee27cd641af7628628ca3549be4f4
ishwarthecodddr <142937211+ishwarthecodddr@users.noreply.github.com>
2024-01-26 13:10:08 +0000

    dates

---
+++ 01_basics/06_datesinjs.js
@@ -0,0 +1,9 @@
+const date = new Date()
+console.log(date)
+console.log(date.toLocaleString())
+console.log(date.toDateString())
+
+let myStamp = Date.now()
+console.log(myStamp);
+console.log(date.getTime())
+console.log()
